d219e92caca8a03825772f351431253328908ab5
mconstant <bobbaxter@protonmail.com>
2024-08-06 19:13:53 -0400

    preliminary kill-listener <PATCH>

--- app/example-deployment/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  // send the env variable MSG
-  res.send('The message is: ' + process.env.TOP_SECRET_MESSAGE);
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
---
+++ app/p2s-kill-listener/kill-listener.py
@@ -0,0 +1,8 @@
+import time
+
+while True:
+    print("listening for fortnite kills")
+    # make a . every second for 10 seconds
+    for i in range(10):
+        time.sleep(1)
+        print(".", end="")
